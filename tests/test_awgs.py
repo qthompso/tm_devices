c1e545902b9b3a46ecbe9835db6dda9cfcede359
qthompso <quinn.thompson@tektronix.com>
2023-10-26 14:54:19 -0700

    updates to device constraints AWG simplified to use model name to generate sample rate unit tests for awg7k, awg70k

--- tests/test_awgs.py
+++ tests/test_awgs.py
@@ -38,28 +38,28 @@ def test_awg5200(device_manager: DeviceManager, capsys: pytest.CaptureFixture[st
         device_manager: The DeviceManager object.
         capsys: The captured stdout and stderr.
     """
-    awg5200_50 = device_manager.add_awg("awg520050-hostname", alias="awg520050")
-    assert id(device_manager.get_awg(number_or_alias="awg5200_50")) == id(awg5200_50)
-    assert id(device_manager.get_awg(number_or_alias=awg5200_50.device_number)) == id(awg5200_50)
-    assert awg5200_50.total_channels == 4
-    assert awg5200_50.all_channel_names_list == ("SOURCE1", "SOURCE2", "SOURCE3", "SOURCE4")
-    awg5200_50.write("*SRE 256")
-    awg5200_50.expect_esr(32, '1, "Command error"\n0,"No error"')
+    awg520050 = device_manager.add_awg("awg520050-hostname", alias="awg520050")
+    assert id(device_manager.get_awg(number_or_alias="awg520050")) == id(awg520050)
+    assert id(device_manager.get_awg(number_or_alias=awg520050.device_number)) == id(awg520050)
+    assert awg520050.total_channels == 4
+    assert awg520050.all_channel_names_list == ("SOURCE1", "SOURCE2", "SOURCE3", "SOURCE4")
+    awg520050.write("*SRE 256")
+    awg520050.expect_esr(32, '1, "Command error"\n0,"No error"')
     with pytest.raises(AssertionError):
-        awg5200_50.expect_esr(32, '1, Command error\n0,"No error"')
-    awg5200_50.load_waveform("test", "file_path.txt", "TXT")
+        awg520050.expect_esr(32, '1, Command error\n0,"No error"')
+    awg520050.load_waveform("test", "file_path.txt", "TXT")
     assert 'MMEMory:IMPort "test", "file_path.txt", TXT' in capsys.readouterr().out
-    awg5200_50.load_waveform("test", '"file_path.txt"', "TXT")
+    awg520050.load_waveform("test", '"file_path.txt"', "TXT")
     assert 'MMEMory:IMPort "test", "file_path.txt", TXT' in capsys.readouterr().out
-    assert awg5200_50.source_device_constants == AWGSourceDeviceConstants(
+    assert awg520050.source_device_constants == AWGSourceDeviceConstants(
         memory_page_size=1,
         memory_max_record_length=16200000,
         memory_min_record_length=1,
     )
-    assert awg5200_50.opt_string == "50"
-    awg520050_constraints = awg5200_50.get_waveform_constraints(SignalSourceFunctionsAWG.SIN)
+    assert awg520050.opt_string == "50"
+    awg520050_constraints = awg520050.get_waveform_constraints(SignalSourceFunctionsAWG.SIN)
     min_smaple_50 = 300.0
-    max_sample_50 = 2.5e9
+    max_sample_50 = 5.0e9
     assert awg520050_constraints == ExtendedSourceDeviceConstants(
         amplitude_range=ParameterRange(min=0.1, max=2.0),
         offset_range=ParameterRange(min=-0.5, max=0.5),
@@ -69,15 +69,15 @@ def test_awg5200(device_manager: DeviceManager, capsys: pytest.CaptureFixture[st
         pulse_width_range=None,
         ramp_symmetry_range=None,
     )
-    awg5200 = device_manager.add_awg("awg520025-hostname", alias="awg520025")
-    assert awg5200_50.opt_string == "25"
-    awg520025_constraints = awg5200.get_waveform_constraints(SignalSourceFunctionsAWG.SQUARE)
+    awg520025 = device_manager.add_awg("awg520025-hostname", alias="awg520025")
+    assert awg520025.opt_string == "25"
+    awg520025_constraints = awg520025.get_waveform_constraints(SignalSourceFunctionsAWG.DC)
     min_smaple_25 = 300.0
     max_sample_25 = 2.5e9
     assert awg520025_constraints == ExtendedSourceDeviceConstants(
         amplitude_range=ParameterRange(min=0.1, max=2.0),
         offset_range=ParameterRange(min=-0.5, max=0.5),
-        frequency_range=ParameterRange(min=min_smaple_25 / 1000.0, max=max_sample_25 / 10.0),
+        frequency_range=ParameterRange(min=min_smaple_25 / 1000.0, max=max_sample_25 / 1000.0),
         sample_rate_range=ParameterRange(min=min_smaple_25, max=max_sample_25),
         square_duty_cycle_range=None,
         pulse_width_range=None,
@@ -112,7 +112,35 @@ def test_awg70k(device_manager: DeviceManager, capsys: pytest.CaptureFixture[str
 
 
 def test_awg7k(device_manager: DeviceManager, capsys: pytest.CaptureFixture[str]) -> None:
-    ampl_range = ParameterRange(min=0.5, max=1.0)
-    offset_range = ParameterRange(min=-0.5, max=0.5)
-    awg70ka150 = device_manager.add_awg("awg7k01-hostname", alias="awg7k01")
-    awg70ka225 = device_manager.add_awg("awg7k06-hostname", alias="awg7k06")
+
+    awg7k01 = device_manager.add_awg("awg7k01-hostname", alias="awg7k01")
+    awg7k06 = device_manager.add_awg("awg7k06-hostname", alias="awg7k06")
+    awg7kb02 = device_manager.add_awg("awg7kb02-hostname", alias="awg7kb02")
+    awg7kb01 = device_manager.add_awg("awg7kb01-hostname", alias="awg7kb01")
+    awg7kc06 = device_manager.add_awg("awg7kc06-hostname", alias="awg7kc06")
+    awg7kc01 = device_manager.add_awg("awg7kc01-hostname", alias="awg7kc01")
+    length_range = ParameterRange(min=10, max=1000)
+    awg_list = [awg7k01, awg7k06, awg7kb02, awg7kb01, awg7kc06, awg7kc01]
+    for awg in awg_list:
+        option = awg.alias[-2:]
+        assert awg.opt_string == option
+
+        sample_range = ParameterRange(min=10.0e6, max=int(awg.model[4:6]) * 1.0e9)
+
+        if option in ("02", "06"):
+            ampl_range = ParameterRange(min=0.5, max=1.0)
+            offset_range = ParameterRange(min=-0.0, max=0.0)
+        else:
+            offset_range = ParameterRange(min=-0.5, max=0.5)
+            ampl_range = ParameterRange(min=50.0e-3, max=2.0)
+
+        constraints = awg.get_waveform_constraints(SignalSourceFunctionsAWG.TRIANGLE)
+
+        check_constraints(
+            constraints,
+            sample_range,
+            ampl_range,
+            offset_range,
+            length_range,
+        )
+
