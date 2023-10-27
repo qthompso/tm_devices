b39d1473c8bce6092c24d552df81452fd65ff708
Le, Kevin <lekevin678@gmail.com>
2023-10-27 13:21:37 -0700

    refactor: awg5k does not have options 02 and 06 so it can use the awg's generate_waveform function.

--- src/tm_devices/drivers/pi/signal_sources/awgs/awg5k.py
+++ src/tm_devices/drivers/pi/signal_sources/awgs/awg5k.py
@@ -22,47 +22,6 @@ class AWG5K(AWG5KMixin, AWG):
     ################################################################################################
     # Public Methods
     ################################################################################################
-    def generate_waveform(  # noqa: PLR0913  # pyright: ignore[reportIncompatibleMethodOverride]
-        self,
-        frequency: float,
-        function: SignalSourceFunctionsAWG,
-        amplitude: float,
-        offset: float,
-        channel: str = "all",
-        burst: int = 0,
-        termination: Literal["FIFTY", "HIGHZ"] = "FIFTY",
-        duty_cycle: float = 50.0,
-        polarity: Literal["NORMAL", "INVERTED"] = "NORMAL",
-        symmetry: float = 50.0,
-    ) -> None:
-        """Generate a signal given the following parameters.
-
-        Args:
-            frequency: The frequency of the waveform to generate.
-            function: The waveform shape to generate.
-            amplitude: The amplitude of the signal to generate.
-            offset: The offset of the signal to generate.
-            channel: The channel name to output the signal from, or 'all'.
-            burst: The number of wavelengths to be generated.
-            termination: The impedance this device's ``channel`` expects to see at the received end.
-            duty_cycle: The duty cycle percentage within [10.0, 90.0].
-            polarity: The polarity to set the signal to.
-            symmetry: The symmetry to set the signal to, only applicable to certain functions.
-        """
-        predefined_name, needed_sample_rate = self._get_predefined_filename(frequency, function)
-        for channel_name in self._validate_channels(channel):
-            source_channel = self.channel[channel_name]
-            self.set_and_check(f"OUTPUT{source_channel.num}:STATE", "0")
-            if needed_sample_rate:
-                first_source_channel = self.channel["SOURCE1"]
-                first_source_channel.set_frequency(round(needed_sample_rate, -1))
-            self._setup_burst_waveform(source_channel.num, predefined_name, burst)
-            source_channel.set_amplitude(amplitude)
-            if not ("02" in self.opt_string or "06" in self.opt_string):
-                source_channel.set_offset(offset)
-            self.set_and_check(f"OUTPUT{source_channel.num}:STATE", "1")
-        self.write("AWGCONTROL:RUN")
-        self.expect_esr(0)
 
     ################################################################################################
     # Private Methods
