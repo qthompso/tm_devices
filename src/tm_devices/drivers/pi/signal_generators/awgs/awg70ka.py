8dfe16304ea3b31f264249ea897978f7af840a23
Le, Kevin <lekevin678@gmail.com>
2024-01-11 10:54:23 -0800

    feat: For awg70k's, default for set_output_path is try to set it to DCA, else set it to DIR.

--- src/tm_devices/drivers/pi/signal_generators/awgs/awg70ka.py
+++ src/tm_devices/drivers/pi/signal_generators/awgs/awg70ka.py
@@ -1,7 +1,8 @@
 """AWG70KA device driver module."""
 from functools import cached_property
+from pathlib import Path
 from types import MappingProxyType
-from typing import Optional, Tuple
+from typing import cast, Optional, Tuple
 
 from tm_devices.commands import AWG70KAMixin
 from tm_devices.drivers.pi.signal_generators.awgs.awg import (
@@ -10,81 +11,77 @@ from tm_devices.drivers.pi.signal_generators.awgs.awg import (
     AWGSourceDeviceConstants,
     ParameterBounds,
 )
+from tm_devices.helpers import SignalSourceOutputPaths
 
 
 class AWG70KAChannel(AWGChannel):
     """AWG70KA channel driver."""
 
-    def set_offset(self, value: float, tolerance: float = 0, percentage: bool = False) -> None:
-        """Set the offset on the source.
+    sample_waveform_file = (
+        "C:\\Program Files\\Tektronix\\AWG70000\\Samples\\AWG5k7k Predefined Waveforms.awgx"
+    )
 
-        Args:
-            value: The offset value to set.
-            tolerance: The acceptable difference between two floating point values.
-            percentage: A boolean indicating what kind of tolerance check to perform.
-                 False means absolute tolerance: +/- tolerance.
-                 True means percent tolerance: +/- (tolerance / 100) * value.
-        """
-        current_high = float(self._pi_device.query(f"{self.name}:VOLTAGE:HIGH?"))
-        current_low = float(self._pi_device.query(f"{self.name}:VOLTAGE:LOW?"))
-
-        current_amplitude = current_high - current_low
-        high_voltage = round(current_amplitude / 2 + value, 3)
-        low_voltage = round(-current_amplitude / 2 + value, 3)
-        self._set_high_and_low_voltage(
-            high_voltage, low_voltage, tolerance=tolerance, percentage=percentage
-        )
-
-    def set_amplitude(self, value: float, tolerance: float = 0, percentage: bool = False) -> None:
-        """Set the amplitude on the source.
-
-        Args:
-            value: The amplitude value to set.
-            tolerance: The acceptable difference between two floating point values.
-            percentage: A boolean indicating what kind of tolerance check to perform.
-                 False means absolute tolerance: +/- tolerance.
-                 True means percent tolerance: +/- (tolerance / 100) * value.
-        """
-        current_high = float(self._pi_device.query(f"{self.name}:VOLTAGE:HIGH?"))
-        current_low = float(self._pi_device.query(f"{self.name}:VOLTAGE:LOW?"))
-
-        current_amplitude = current_high - current_low
-        offset = current_high - (current_amplitude / 2)
-        high_voltage = round(value / 2 + offset, 3)
-        low_voltage = round(-value / 2 + offset, 3)
-        self._set_high_and_low_voltage(
-            high_voltage, low_voltage, tolerance=tolerance, percentage=percentage
-        )
-
-    def _set_high_and_low_voltage(
+    def load_waveform(
         self,
-        high_voltage: float,
-        low_voltage: float,
-        tolerance: float = 0,
-        percentage: bool = False,
+        waveform_file: str,
+        waveform: Optional[str] = None,
     ) -> None:
-        """Set the HIGH and LOW voltage to the specified values.
+        """Load in all waveforms or a specific waveform from a waveform file.
+
+        Arguments:
+            waveform_file: The waveform file to load.
+            waveform: The specific waveform to load from the waveform file.
+        """
+        waveform_file_type = Path(waveform_file).suffix.lower()
+        if waveform_file_type not in [".awg", ".awgx", ".mat", ".seqx"]:
+            waveform_file_type_error = (
+                f"{waveform_file_type} is an invalid waveform file extension."
+            )
+            raise ValueError(waveform_file_type_error)
+        if not waveform:
+            self._awg.write(command=f'MMEMORY:OPEN:SASSET "{waveform_file}"', opc=True)
+        else:
+            self._awg.write(
+                command=f'MMEMORY:OPEN:SASSET:WAVEFORM "{waveform_file}", "{waveform}"', opc=True
+            )
+
+    def set_frequency(self, value: float, tolerance: float = 0, percentage: bool = False) -> None:
+        """Set the frequency on the source.
 
         Args:
-            high_voltage: The HIGH voltage value to set.
-            low_voltage: The LOW voltage value to set.
+            value: The frequency value to set.
             tolerance: The acceptable difference between two floating point values.
             percentage: A boolean indicating what kind of tolerance check to perform.
                  False means absolute tolerance: +/- tolerance.
                  True means percent tolerance: +/- (tolerance / 100) * value.
         """
-        self._pi_device.set_if_needed(
-            f"{self.name}:VOLTAGE:HIGH",
-            high_voltage,
-            tolerance=tolerance,
-            percentage=percentage,
+        self._awg.set_if_needed(
+            f"{self.name}:FREQUENCY", value, tolerance=tolerance, percentage=percentage, opc=True
         )
-        self._pi_device.set_if_needed(
-            f"{self.name}:VOLTAGE:LOW",
-            low_voltage,
-            tolerance=tolerance,
-            percentage=percentage,
-        )
+
+    def set_output_path(self, value: Optional[SignalSourceOutputPaths] = None) -> None:
+        """Set the output signal path on the source.
+
+        Args:
+            value: The output signal path.
+        """
+        if not value:
+            try:
+                self._awg.set_and_check(f"OUTPUT{self.num}:PATH", SignalSourceOutputPaths.DCA.value)
+            except AssertionError:
+                expected_esr_message = (
+                    '-222,"Data out of range;Data Out of Range - '
+                    'OUTPUT1:PATH DCA\r\n"\n0,"No error"'
+                )
+                self._awg.expect_esr("16", expected_esr_message)
+                self._awg.set_and_check(f"OUTPUT{self.num}:PATH", SignalSourceOutputPaths.DIR.value)
+        elif value in [SignalSourceOutputPaths.DIR, SignalSourceOutputPaths.DCA]:
+            self._awg.set_if_needed(f"OUTPUT{self.num}:PATH", value.value)
+        else:
+            output_signal_path_error = (
+                f"{value.value} is an invalid output signal path for {self._awg.model}."
+            )
+            raise ValueError(output_signal_path_error)
 
 
 class AWG70KA(AWG70KAMixin, AWG):
@@ -110,6 +107,43 @@ class AWG70KA(AWG70KAMixin, AWG):
     ################################################################################################
     # Public Methods
     ################################################################################################
+    def set_waveform_properties(  # noqa: PLR0913
+        self,
+        source_channel: AWGChannel,
+        output_path: Optional[SignalSourceOutputPaths],
+        predefined_name: str,
+        needed_sample_rate: float,
+        amplitude: float,
+        offset: float,
+        burst: int,
+    ) -> None:
+        """Set the properties of the waveform.
+
+        Args:
+            source_channel: The source channel class for the requested channel.
+            output_path: The output signal path of the specified channel.
+            predefined_name: The name of the function to generate.
+            needed_sample_rate: The required sample
+            amplitude: The amplitude of the signal to generate.
+            offset: The offset of the signal to generate.
+
+            burst: The number of wavelengths to be generated.
+        """
+        source_channel = cast(AWG70KAChannel, source_channel)
+        source_channel.load_waveform(waveform_file=source_channel.sample_waveform_file)
+        super().set_waveform_properties(
+            source_channel=source_channel,
+            output_path=output_path,
+            predefined_name=predefined_name,
+            needed_sample_rate=needed_sample_rate,
+            amplitude=amplitude,
+            offset=offset,
+            burst=burst,
+        )
+
+    ################################################################################################
+    # Private Methods
+    ################################################################################################
     def _get_series_specific_constraints(
         self,
         output_path: Optional[SignalSourceOutputPaths],
@@ -126,7 +160,7 @@ class AWG70KA(AWG70KAMixin, AWG):
             offset_range = ParameterBounds(lower=-0.0, upper=0.0)
 
         rates = ["50", "25", "16", "08"]
-        max_sample_rate = [rate for rate in rates if rate in self.opt_string][0]
+        max_sample_rate = [rate for rate in rates if rate in self.opt_string][0]  # noqa: RUF015
         # first digit indicates the number of channels, second and third indicate sample rate (GHz)
         sample_rate_range = ParameterBounds(lower=1.5e3, upper=int(max_sample_rate) * 1.0e9)
         return amplitude_range, offset_range, sample_rate_range
