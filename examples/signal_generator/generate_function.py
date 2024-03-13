2d6ca69d6a8d3d40e1fd7d9f2346f24f36ba2c7c
Le, Kevin <lekevin678@gmail.com>
2024-03-13 16:15:54 -0700

    docs: use device constants to reference function in generate_function.py example rather than import;

--- examples/signal_generator/generate_function.py
+++ examples/signal_generator/generate_function.py
@@ -2,7 +2,6 @@
 
 from tm_devices import DeviceManager
 from tm_devices.drivers import AFG3K, AWG5K
-from tm_devices.helpers import SignalGeneratorFunctionsAFG, SignalGeneratorFunctionsAWG
 
 with DeviceManager(verbose=True) as dm:
     # Create a connection to the AFG and indicate that it is an AFG3K for type hinting
@@ -13,7 +12,7 @@ with DeviceManager(verbose=True) as dm:
 
     # Generate a RAMP waveform on SOURCE1 of the AFG3K with the provided properties.
     afg3k.generate_function(
-        function=SignalGeneratorFunctionsAFG.RAMP,
+        function=afg3k.source_device_constants.functions.RAMP,
         channel="SOURCE1",
         frequency=10e5,
         amplitude=0.5,
@@ -23,7 +22,7 @@ with DeviceManager(verbose=True) as dm:
 
     # Generate a SIN waveform on all channels of the AWG5K with the provided properties.
     awg5k.generate_function(
-        function=SignalGeneratorFunctionsAWG.SIN,
+        function=awg5k.source_device_constants.functions.SIN,
         channel="all",
         frequency=10e5,
         amplitude=0.5,
